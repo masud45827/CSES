63ef1d8af99b2a7301d3aecad2da59abd995b5b9
masud45827 <60808203+masud45827@users.noreply.github.com>
2022-01-21 22:28:28 -0800

    Create Rectangle Cutting.cpp

---
+++ Rectangle Cutting.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef     long long    ll;
+//typedef     unsigned long long    ull;
+typedef     vector<int> vi;
+typedef     vector<long long> vl;
+typedef     pair<int, int>pi;
+typedef     pair<long long, long long>pl;
+#define F   first
+#define S   second
+#define pb  push_back
+#define     all(x)      x.begin() , x.end()
+#define mod 1000000007
+#define       FOR(i,a,b) for(i=a;i<=b;i++)
+#define     mem(a)      memset(a , 0 ,sizeof a)
+#define     memn(a)     memset(a , -1 ,sizeof a)
+#ifdef LOCAL
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {cout << endl ;}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+  cerr << *it << " = " << a << ' ' ;
+  err(++it, args...);
+}
+#else
+#define error(args...)
+#endif
+int dp[505][505];
+int main() {
+  int test = 1, fac = 1;
+  //scanf("%d", &test);
+  while (test--) {
+    int n,m, i, j;
+    cin >> n >> m;
+    for (i = 0; i <= n; i++) {
+      for (j = 0; j <= m; j++) {
+        if (i == j) continue;
+        dp[i][j] = 1e9;
+        for (int k = 1; k < i; k++) {
+          dp[i][j] = min(dp[i][j], dp[k][j] + dp[i - k][j] + 1);
+        }
+        for (int k = 1; k < j; k++) {
+          dp[i][j] = min(dp[i][j], dp[i][k] + dp[i][j - k] + 1);
+        }
+      }
+    }
+    cout<<dp[n][m]<<endl;
+  }
+  return 0;
+}
