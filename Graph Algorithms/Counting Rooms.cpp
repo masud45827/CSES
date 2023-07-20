5f366cffba18cdfd78c1740d7fa8e9622b3c4d6f
masud45827 <60808203+masud45827@users.noreply.github.com>
2023-07-20 12:23:25 +0600

    Update Counting Rooms.cpp

--- Graph Algorithms/Counting Rooms.cpp
+++ Graph Algorithms/Counting Rooms.cpp
@@ -15,55 +15,56 @@ typedef     pair<long long, long long>pl;
 #define     mem(a)      memset(a , 0 ,sizeof a)
 #define     memn(a)     memset(a , -1 ,sizeof a)
 char ch[1004][1004];
-void dfs(int i,int j)
+void dfs(int i, int j)
 {
-     if(ch[i-1][j]=='.')
-     {
-        ch[i-1][j]='#';
-        dfs(i-1,j);
-     }
-     if(ch[i+1][j]=='.')
-     {
-        ch[i+1][j]='#';
-        dfs(i+1,j);
-     }
-     if(ch[i][j-1]=='.')
-     {
-        ch[i][j-1]='#';
-        dfs(i,j-1);
-     }
-     if(ch[i][j+1]=='.')
-     {
-        ch[i][j+1]='#';
-        dfs(i,j+1);
-     }
- 
+  if (ch[i - 1][j] == '.')
+  {
+    ch[i - 1][j] = '#';
+    dfs(i - 1, j);
+  }
+  if (ch[i + 1][j] == '.')
+  {
+    ch[i + 1][j] = '#';
+    dfs(i + 1, j);
+  }
+  if (ch[i][j - 1] == '.')
+  {
+    ch[i][j - 1] = '#';
+    dfs(i, j - 1);
+  }
+  if (ch[i][j + 1] == '.')
+  {
+    ch[i][j + 1] = '#';
+    dfs(i, j + 1);
+  }
+
 }
 int main()
 {
-    int t = 1, fac = 1;
-    //  scanf("%d", &t);
-    while (t--)
-    {
-        int n,m,i,x,y,c=0,j;
-        cin>>n>>m;
-        for(i=0;i<=m+1;i++) ch[0][i]='#';
-         for(j=0;j<=n+1;j++) ch[j][i]='#';
-            for(i=1;i<=n;i++)
-            {
-                for(j=1;j<=m;j++) cin>>ch[i][j];
-            }
-        for(i=1;i<=n;i++)
-        {
-            for(j=1;j<=n;j++)
-            {
-                if(ch[i][j]=='.') 
-                    {
-                        c++;
-                        dfs(i,j);
-                    }
-            }
-        }
-        cout<<c<<endl;
+  int t = 1, fac = 1;
+  //  scanf("%d", &t);
+  while (t--)
+  {
+    int n, m, i, x, y, c = 0, j;
+    cin >> n >> m;
+    for (i = 0; i <= n + 2; i++) {
+      for (j = 0; j <= m + 2; j++) ch[i][j] = '#';
     }
+    for (i = 1; i <= n; i++)
+    {
+      for (j = 1; j <= m; j++) cin >> ch[i][j];
+    }
+    for (i = 1; i <= n; i++)
+    {
+      for (j = 1; j <= m; j++)
+      {
+        if (ch[i][j] == '.')
+        {
+          c++;
+          dfs(i, j);
+        }
+      }
+    }
+    cout << c << endl;
+  }
 }
